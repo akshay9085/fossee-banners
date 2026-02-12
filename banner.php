029b65780e8a099c63cc1add59e9b0701b250e46
akshay <117985209+akshay9085@users.noreply.github.com>
2026-02-12 18:09:36 +0530

    Update date condition for displaying banner

--- banner.php
+++ banner.php
@@ -4,7 +4,7 @@ $hackathon_date = "2025-02-5 23:59:59.0";
 $autumn_date = "2025-10-27 23:59:59.0";
 $oshw_hackathon_date = "2026-03-16 23:59:59.0";
 $semester_internship_date = "2026-02-10 23:59:59.0";
-if($today<$oshw_hackathon_date){
+if($today<$semester_internship_date){
 echo '   <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/nice-bar.css" />     
           <link rel="stylesheet" type="text/css" href="https://fossee.in/data/banner/css/lightbox.css" /> 
            <script type="text/javascript" src="https://fossee.in/data/banner/js/nice-bar.js"></script>
